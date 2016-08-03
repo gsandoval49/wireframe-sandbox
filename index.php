4f43da7de7f9ca7a6471f1f7968e845cbc38eb0f
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-03 02:26:08 -0600

    added div classes for wire framing techniques

--- index.php
+++ index.php
@@ -30,25 +30,12 @@
         <title>Wireframe Sandbox</title>
     </head>
     <body>
-        <!-- Title will go here and the wireframing will begin-->
-        <h1>example test</h1>
-        <!-- Stack the columns on mobile by making one full-width and the other half-width -->
-        <div class="row">
-            <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
-            <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+        <div class="wrapper">
+            <header></header>
+            <div id="container"></div>
+            <footer></footer>
         </div>
 
-        <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-        <div class="row">
-            <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-            <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-            <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-        </div>
 
-        <!-- Columns are always 50% wide, on mobile and desktop -->
-        <div class="row">
-            <div class="col-xs-6">.col-xs-6</div>
-            <div class="col-xs-6">.col-xs-6</div>
-        </div>
     </body>
 </html>
