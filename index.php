3f284ca3221a77e719f1a87be931af9b72f2c656
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-03 01:49:18 -0600

    added bootstrap to html file - latest version

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html>
     <head>
+
+        <!-- universal language -->
         <meta charset="utf-8"/>
+
+        <!-- content from class materials/bootstrap code to incorporate -->
+        <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- latest compiled and minified CSS -->
+        <!-- taken from this website https://www.bootstrapcdn.com -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+        <!--css goes here -->
+        <link type="text/css" href="css/stylesheet.css" rel="stylesheet"
+
+        <!-- title here -->
         <title>Wireframe Sandbox</title>
     </head>
     <body>
