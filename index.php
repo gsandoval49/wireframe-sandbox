7bab87953252c8012d274c29faf973da98603634
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-03 02:10:18 -0600

    added option theme for bootstrap to html file

--- index.php
+++ index.php
@@ -13,9 +13,12 @@
         <!-- taken from this website https://www.bootstrapcdn.com -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
 
         <!--css goes here -->
-        <link type="text/css" href="css/stylesheet.css" rel="stylesheet"
+        <link type="text/css" href="css/stylesheet.css" rel="stylesheet">
+
 
         <!-- title here -->
         <title>Wireframe Sandbox</title>
