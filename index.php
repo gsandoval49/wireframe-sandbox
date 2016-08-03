3c16d9d9bbe070ec7e475abcb860f6f1ba14639c
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-03 00:52:49 -0600

    html content framing built

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Wireframe Sandbox</title>
+    </head>
+    <body>
+        <!-- Title will go here and the wireframing will begin-->
+        <h1>example test</h1>
+    </body>
+</html>
