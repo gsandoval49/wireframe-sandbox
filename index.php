c562bf3f1039f33b197a136c16f0410210e2523a
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-04 16:31:42 -0600

    bootstrap metas, scripts, links

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
-
-        <!-- universal language -->
-        <meta charset="utf-8"/>
-
-        <!-- content from class materials/bootstrap code to incorporate -->
-        <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="IE-edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- latest compiled and minified CSS -->
-        <!-- taken from this website https://www.bootstrapcdn.com -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- OPTIONAL THEME HERE -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+        <!-- Latest compiled and minified CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <!--css goes here -->
-        <link type="text/css" href="css/stylesheet.css" rel="stylesheet">
+        <!-- ADD YOUR CUSTOM CSS HERE -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+
+        <!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
@@ -25,20 +27,7 @@
         <!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-        <!-- GOOGLE FONTS HERE -->
-        <link href="https://fonts.googleapis.com/css?family=Lato|Source+Sans+Pro" rel="stylesheet">
 
-        <!-- title here -->
-        <title>Wireframe Sandbox</title>
     </head>
-    <body>
-            <header>
-                <h1>test with google fonts</h1>
-                    <p>test witih stuff here for google fotns</p>
-            </header>
-            <footer></footer>
-        </div>
 
-
-    </body>
 </html>
