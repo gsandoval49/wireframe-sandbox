432ed25902ee3d943fcb5300cadfe7092040cd2d
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-04 22:45:35 -0600

    renamed buttons

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                                     <span class="icon-bar"></span>
                                     <span class="icon-bar"></span>
                                 </button>
-                                <a class="navbar-brand" href="#">My Bootstrap Page</a>
+                                <a class="navbar-brand" href="#">Logo Here</a>
                             </div>
 
                             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -62,11 +62,11 @@
                                     <li class="dropdown">
                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Moar Links <span class="caret"></span></a>
                                         <ul class="dropdown-menu">
-                                            <li><a href="#">Action</a></li>
-                                            <li><a href="#">Another action</a></li>
-                                            <li><a href="#">Something else here</a></li>
+                                            <li><a href="#">Experiences</a></li>
+                                            <li><a href="#">Latest Projects</a></li>
+                                            <li><a href="#">Clients</a></li>
                                             <li role="separator" class="divider"></li>
-                                            <li><a href="#">Separated link</a></li>
+                                            <li><a href="#">Get in Touch</a></li>
                                         </ul>
                                     </li>
                                 </ul>
