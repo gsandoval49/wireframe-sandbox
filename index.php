a06ca8814be16801d4296ec5c188a0ab72e91803
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-14 19:48:15 -0600

    changed optional theme after latest compiled css - suggestions from Rochelle

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
         <meta http-equiv="x-ua-compatible" content="IE-edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- OPTIONAL THEME HERE -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
         <!-- Latest compiled and minified CSS-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- OPTIONAL THEME HERE -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
         <!-- ADD YOUR CUSTOM CSS HERE -->
         <link rel="stylesheet" href="css/stylesheet.css" type="text/css">
